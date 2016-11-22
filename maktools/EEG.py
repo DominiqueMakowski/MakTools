6d122e91a4f6e2d3db6cfc636ac4187c7f20adb2
Dominique Makowski <dom.mak19@gmail.com>
2016-11-22 17:32:26 +0100

    eeg

--- maktools/EEG.py
+++ maktools/EEG.py
@@ -94,7 +94,7 @@ def add_events(raw, participant, path="data/", stimdata_extension=".xlsx", exper
 # ==============================================================================
 # ==============================================================================
 # ==============================================================================
-def load_eeg(participant, path="data/", experiment="", system="brainvision", reference=None, stimdata_extension=".xlsx", stim_channel="PHOTO", treshold=0.04, upper=False, number=45, pause=None, after=0, before=None, condition1=None, condition2=None):
+def eeg_load(participant, path="data/", experiment="", system="brainvision", reference=None, stimdata_extension=".xlsx", stim_channel="PHOTO", treshold=0.04, upper=False, number=45, pause=None, after=0, before=None, condition1=None, condition2=None):
     """
     """
     raw = load_brainvision_raw(participant, path=path, experiment=experiment, system=system, reference=reference)
@@ -113,3 +113,88 @@ def load_eeg(participant, path="data/", experiment="", system="brainvision", ref
                                            condition1=condition1,
                                            condition2=condition2)
     return(raw, events, event_id)
+    
+# ==============================================================================
+# ==============================================================================
+# ==============================================================================
+# ==============================================================================
+# ==============================================================================
+# ==============================================================================
+# ==============================================================================
+# ==============================================================================
+def eeg_plot_all(raw, events, event_id, eog_reject=600e-6, save=True, step="", topo=False):
+    reject = {
+#        "eeg": 4000e-13,
+        "eog": eog_reject  # Adjust with caution
+        }
+    # picks
+    picks = mne.pick_types(raw.info,
+                       meg=False,
+                       eeg=True,
+                       eog=True,
+                       stim=False,
+                       exclude='bads',
+#                       selection=O_cluster + PO_cluster
+                       )
+
+    # epochs
+    epochs = mne.Epochs(raw,
+                        events=events,
+                        event_id=event_id,
+                        tmin=-0.2,
+                        tmax=1,
+                        picks=picks,
+                        add_eeg_ref=True,
+                        reject_by_annotation=True,
+                        reject=reject,  # Adjust values carefully
+                        proj=True,  # With SSP projections
+                        detrend=1,  # "None", 1: Linear detrend, 0 DC detrend,
+                        baseline=(None, 0)  #
+                        )
+    
+    # Drop bads
+    epochs.drop_bad()
+    
+    
+    # Plot
+    if topo is False:
+        fig = mne.combine_evoked([epochs.average()]).plot_joint()
+        if save is True:
+            fig.savefig('plots/all_' + str(step) +  '.png', format='png', dpi=1000)
+        
+        
+    if topo is True:
+        fig = mne.viz.plot_evoked_topo([epochs.average()],
+        #                                  fig_background="black",
+                                          fig_facecolor="black",
+        #                                  conditions = ['Negative', 'Neutral'],
+    #                                      scalings=dict(eeg=1e1),
+                                          layout=mne.channels.find_layout(raw.info),
+                                          font_color="black",
+                                          axis_facecolor="black",
+    #                                      proj = "interactive",
+                                          color='red'
+        #                                  layout_scale = 2
+                                          )
+        if save is True:
+            fig.savefig('plots/all_' + str(step) +  '.png', format='png', dpi=1000)
+            
+            
+# ==============================================================================
+# ==============================================================================
+# ==============================================================================
+# ==============================================================================
+# ==============================================================================
+# ==============================================================================
+# ==============================================================================
+# ==============================================================================
+def eeg_filter(raw, notch=True, method="iir"):
+    if notch == True:
+        raw.notch_filter(np.arange(50, 201, 50),
+                         phase='zero',
+                         method=method
+                         )
+    return(raw)
+    
+    
+    
