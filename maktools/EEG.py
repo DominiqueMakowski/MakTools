25b6261daeaafe2a8a5dc6131979f3dea6983b29
DominiqueMakowski <dom.mak19@gmail.com>
2016-11-23 12:02:24 +0100

    eeg

--- maktools/EEG.py
+++ maktools/EEG.py
@@ -338,7 +338,7 @@ def eeg_epoching(raw, events, event_id, tmin=-0.2, tmax=1, eog_reject=600e-6, pr
 # ==============================================================================
 # ==============================================================================
 # ==============================================================================
-def eeg_fractal_dim(epochs):
+def eeg_fractal_dim(epochs, entropy=True, hurst=True, dfa=False, lyap_r=False, lyap_e=False):
     """
     """
 
@@ -356,31 +356,65 @@ def eeg_fractal_dim(epochs):
 
 
 
-    data = {
-            "Entropy": {},
-#            "hurst": {}
-            }
+    data = {}
+    if entropy == True:
+        data["Entropy"] = {}
+    if hurst == True:
+        data["Hurst"] = {}
+    if dfa == True:
+        data["DFA"] = {}
+    if lyap_r == True:
+        data["Lyapunov_R"] = {}
+    if lyap_e == True:
+        data["Lyapunov_E"] = {}
+
 
     n.time.reset()
     for epoch in set(epochs):
         subset = df.loc[epoch]
 
-        data["Entropy"][epoch] = []
-#        data["hurst"][epoch] = []
+        if entropy == True:
+            data["Entropy"][epoch] = []
+        if hurst == True:
+            data["Hurst"][epoch] = []
+        if dfa == True:
+            data["DFA"][epoch] = []
+        if lyap_r == True:
+            data["Lyapunov_R"][epoch] = []
+        if lyap_e == True:
+            data["Lyapunov_E"][epoch] = []
+
+
 
         for channel in subset:
-            data["Entropy"][epoch].append(nolds.sampen(subset[channel]))
-#            data["hurst"][epoch].append(nolds.hurst_rs(subset[channel]))
-        data["Entropy"][epoch] = np.mean(data["Entropy"][epoch])
-#        data["hurst"][epoch] = np.mean(data["hurst"][epoch])
+            if entropy == True:
+                data["Entropy"][epoch].append(nolds.sampen(subset[channel]))
+            if hurst == True:
+                data["Hurst"][epoch].append(nolds.hurst_rs(subset[channel]))
+            if dfa == True:
+                data["DFA"][epoch].append(nolds.dfa(subset[channel]))
+            if lyap_r == True:
+                data["Lyapunov_R"][epoch].append(nolds.lyap_r(subset[channel]))
+            if lyap_e == True:
+                data["Lyapunov_E"][epoch].append(nolds.lyap_e(subset[channel]))
+
+        if entropy == True:
+            data["Entropy"][epoch] = np.mean(data["Entropy"][epoch])
+        if hurst == True:
+            data["Hurst"][epoch] = np.mean(data["Hurst"][epoch])
+        if dfa == True:
+            data["DFA"][epoch] = np.mean(data["DFA"][epoch])
+        if lyap_r == True:
+            data["Lyapunov_R"][epoch] = np.mean(data["Lyapunov_R"][epoch])
+        if lyap_e == True:
+            data["Lyapunov_E"][epoch] = np.mean(data["Lyapunov_E"][epoch])
 
 
-
-        print(str(round((epoch+1)/len(set(epochs))*100,2)) + "% Complete")
         time = n.time.get(reset=False)/1000
         time = time/(epoch+1)
         time = (time * (len(set(epochs))-epoch))/60
-        print("Remaining time: " + str(round(time, 2)) + 'min')
+        print(str(round((epoch+1)/len(set(epochs))*100,2)) + "% complete, remaining time: " + str(round(time, 2)) + 'min')
 
+    df["Epoch"] = list(set(events))
     df = pd.DataFrame.from_dict(data)
     return(df)
